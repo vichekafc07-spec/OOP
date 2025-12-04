abdad8987bd3eb02c6d2e188a689e42390c21208
Vicheka Sorl <vichekafc07@gmial.com>
2025-12-04 15:04:38 +0700

    member testing

---
+++ src/array_list/Main.java
@@ -0,0 +1,13 @@
+package array_list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Sorl Vichika");
+
+        System.out.println(names);
+    }
+}
